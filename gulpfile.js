a7e8da0660601801af5ecd255bcf051c8b265f5d
softwarespot <softwarespotapps@gmail.com>
2015-08-22 12:32:27 +0300

    Updated build script

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,23 @@
 var gulp = require('gulp');
-var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
 var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 var del = require('del');
 
+// Clean the current directory
 gulp.task('clean', function (cb) {
     del(['jquery-handlebars.min.js'], cb);
 });
 
+// Check the code meets the following standards outlined in .jshintrc
 gulp.task('jshint', function () {
     return gulp.src('./jquery-handlebars.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
-gulp.task('scripts', ['clean', 'jshint'], function () {
+// Uglify aka minify the main file
+gulp.task('uglify', ['clean'], function () {
     return gulp.src('./jquery-handlebars.js')
         .pipe(uglify({
             // See the uglify documentation for more details
@@ -31,8 +34,13 @@ gulp.task('scripts', ['clean', 'jshint'], function () {
         .pipe(gulp.dest('./'))
 });
 
+// Watch for changes to the main file to trigger hinting and minification
 gulp.task('watch', function () {
-    gulp.watch('./jquery-handlebars.js', ['scripts']);
+    gulp.watch('./jquery-handlebars.js', ['jshint', 'uglify']);
 });
 
-gulp.task('default', ['scripts']);
+// Register the default task to hinting and minification
+gulp.task('default', ['jshint', 'uglify']);
+
+// 'gulp jshint' to check the syntax
+// 'gulp uglify' to uglify the main file
