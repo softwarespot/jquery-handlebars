d7dfc314715a2bce8a330d4ce2c3f036cc07da42
softwarespot <softwarespotapps@gmail.com>
2015-08-22 12:16:39 +0300

    Updated gulpfile.js with uglify options

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,34 @@ var jshint = require('gulp-jshint');
 var rename = require('gulp-rename');
 var del = require('del');
 
-gulp.task('clean', function(cb) {
+gulp.task('clean', function (cb) {
     del(['jquery-handlebars.min.js'], cb);
 });
 
-gulp.task('jshint', function() {
+gulp.task('jshint', function () {
     return gulp.src('./jquery-handlebars.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
-gulp.task('scripts', ['clean', 'jshint'], function() {
+gulp.task('scripts', ['clean', 'jshint'], function () {
     return gulp.src('./jquery-handlebars.js')
-        .pipe(uglify())
+        .pipe(uglify({
+            // See the uglify documentation for more details
+            compress: {
+                comparisons: true,
+                conditionals: true,
+                dead_code: true,
+                drop_console: true,
+                unsafe: true,
+                unused: true
+            }
+        }))
         .pipe(rename('jquery-handlebars.min.js'))
         .pipe(gulp.dest('./'))
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch('./jquery-handlebars.js', ['scripts']);
 });
 
