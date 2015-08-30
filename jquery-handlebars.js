6387882c5bd63bc14693c236941656e210277aa2
softwarespot <softwarespotapps@gmail.com>
2015-08-30 15:31:46 +0300

    Updated to v1.1.3

--- jquery-handlebars.js
+++ jquery-handlebars.js
@@ -5,7 +5,7 @@
  * https://github.com/softwarespot/jquery-handlebars
  * Author: softwarespot
  * Licensed under the MIT license
- * Version: 1.1.2
+ * Version: 1.1.3
  */
 ; // jshint ignore:line
 (function ($, undefined) {
