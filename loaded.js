936fc471b04be227b8a5f6ab9cabc7d4aab98cb3
softwarespot <softwarespotapps@gmail.com>
2016-04-17 23:52:31 +0300

    Updated example

---
+++ loaded.js
@@ -0,0 +1,5 @@
+<!--This will NOT be removed from the DOM-->
+<script id="loaded-template" type="text/x-handlebars-template">
+    Hello, {{name}}, how are you? My name is {{developer}} and I am a developer.
+    <br/>
+</script>
